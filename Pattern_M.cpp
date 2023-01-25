645f89997d935b697f75d9011af5440543e2aa95
Gaurishh <98723006+Gaurishh@users.noreply.github.com>
2023-01-25 23:18:47 +0530

    Add files via upload

---
+++ Pattern_M.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    cin >> n;
+
+    int i;
+    int row;
+
+    for(i=1;i<=2*n-1;i++){
+        cout << "*";
+    }
+//
+
+    cout << endl;
+
+    for(row=1;row<=n-1;row++){
+
+        for(i=1;i<=n-row;i++){
+            cout << "*";
+        }
+
+        for(i=1;i<=2*row-1;i++){
+            cout << " ";
+        }
+
+        for(i=1;i<=n-row;i++){
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    int a;
+    for(row=1;row<=n-2;row++){
+
+        for(i=1;i<=row+1;i++){
+            cout << "*";
+        }
+
+        a=n-(row+1);
+
+        for(i=1;i<=2*a-1;i++){
+            cout << " ";
+        }
+
+        for(i=1;i<=row+1;i++){
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+
+    for(i=1;i<=2*n-1;i++){
+        cout << "*";
+    }
+
+}
